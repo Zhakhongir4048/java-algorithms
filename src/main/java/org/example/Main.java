58a072c3deef50650e74a4531dbb228cdccf19f7
Эсонов Жахонгир <dzhoha4048@mal.ru>
2024-06-04 19:57:57 +0300

    Graph

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.chapter_13.Graph;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Graph graph = new Graph();
+        graph.addVertex('F');
+        graph.addVertex('T');
+        graph.addVertex('U');
+        graph.addVertex('Z');
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 1);
+        graph.displayVertex(3);
     }
 }
