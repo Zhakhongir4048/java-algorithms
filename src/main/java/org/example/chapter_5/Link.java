72daef50b0480e9be9849827cb65ffa3c311d670
Эсонов Жахонгир <dzhoha4048@mal.ru>
2024-08-03 12:26:37 +0300

    Глава 5

---
+++ src/main/java/org/example/chapter_5/Link.java
@@ -0,0 +1,17 @@
+package org.example.chapter_5;
+
+public class Link {
+
+    public int iData; // Данные
+    public double dData; // Данные
+    public Link next; // Следующий элемент в списке
+
+    public Link(int iData, double dData) { // Конструктор
+        this.iData = iData; // Инициализация данных
+        this.dData = dData; // (‘next’ автоматически присваивается null)
+    }
+
+    public void displayLink() { // Вывод содержимого элемента
+        System.out.print(STR."{\{iData}, \{dData}} ");
+    }
+}
