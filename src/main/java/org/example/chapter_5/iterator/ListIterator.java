f4754d3d2437f3f5949d7b34283df8068a8bbcf4
Эсонов Жахонгир <dzhoha4048@mal.ru>
2024-08-31 10:28:49 +0300

    Глава 5

--- src/main/java/org/example/chapter_5/iterator/ListIterator.java
+++ src/main/java/org/example/chapter_5/iterator/ListIterator.java
@@ -6,64 +6,64 @@ import java.io.InputStreamReader;
 
 class ListIterator {
 
-    private Link current;
-    private Link previous;
-    private LinkList ourList;
+    private Link current; // Текущий элемент списка
+    private Link previous; // Предыдущий элемент списка
+    private LinkList ourList; // Связанный список
 
-    ListIterator(LinkList list) {
+    ListIterator(LinkList list) { // Конструктор
         this.ourList = list;
-
+        reset();
     }
 
-    void reset() {
+    void reset() { // Возврат к 'first'
         current = ourList.getFirst();
         previous = null;
     }
 
-    boolean atEnd() {
-        return current.next == null;
+    boolean atEnd() { // true, если текущим является
+        return (current.next == null); // последний элемент
     }
 
-    void nextLink() {
+    void nextLink() { // Переход к следующему элементу
         previous = current;
         current = current.next;
     }
 
-    Link getCurrent() {
-        return this.current;
+    Link getCurrent() { // Получение текущего элемента
+        return current;
     }
 
-    void insertAfter(long dData) {
+    void insertAfter(long dData) { // Вставка после текущего элемента
         Link newLink = new Link(dData);
-        if (ourList.isEmpty()) {
+        if (ourList.isEmpty()) { // Пустой список
             ourList.setFirst(newLink);
             current = newLink;
-        } else {
+        } else { // Список не пуст
             newLink.next = current.next;
             current.next = newLink;
-            nextLink();
+            nextLink(); // Переход к новому элементу
         }
     }
 
-    void insertBefore(long dData) {
+    void insertBefore(long dData) { // Вставка перед текущим элементом
         Link newLink = new Link(dData);
-        if (previous == null) {
+        if (previous == null) { // В начале списка (или пустой список)
             newLink.next = ourList.getFirst();
             ourList.setFirst(newLink);
             reset();
-        } else {
+        } else { // Не в начале списка
             newLink.next = previous.next;
             previous.next = newLink;
             current = newLink;
         }
     }
 
-    long deleteCurrent() {
+    long deleteCurrent() { // Удаление текущего элемента
         long value = current.dData;
-        if (previous == null) {
+        if (previous == null) { // Если в начале списка
             ourList.setFirst(current.next);
             reset();
-        } else {
+        } else { // Не в начале списка
             previous.next = current.next;
             if (atEnd())
                 reset();
@@ -74,61 +74,60 @@ class ListIterator {
     }
 
     public static void main(String[] args) throws IOException {
-        LinkList theList = new LinkList(); //
-        ListIterator iter1 = theList.getIterator();
+        LinkList theList = new LinkList(); // Создание списка
+        ListIterator iter1 = theList.getIterator(); // Создание итератора
         long value;
 
-        iter1.insertAfter(20);
+        iter1.insertAfter(20); // Вставка элементов
         iter1.insertAfter(40);
         iter1.insertAfter(80);
-        iter1.insertAfter(60);
+        iter1.insertBefore(60);
 
         while (true) {
             System.out.print("Enter first letter of show, reset, ");
             System.out.print("next, get, before, after, delete: ");
             System.out.flush();
-            int choice = getChar();
+            int choice = getChar(); // Ввод команды
             switch (choice) {
-                case 's' -> {
+                case 's' -> { // Вывод списка
                     if (!theList.isEmpty())
                         theList.displayList();
                     else
                         System.out.println("List is empty");
                 }
-                case 'r' -> iter1.reset();
-                case 'n' -> {
+                case 'r' -> iter1.reset(); // Возврат к первому элементу
+                case 'n' -> { // Переход к следующему элементу
                     if (!theList.isEmpty() && !iter1.atEnd()) {
                         iter1.nextLink();
                     } else {
                         System.out.println("Can’t go to next link");
                     }
                 }
-                case 'g' -> {
+                case 'g' -> { // Получение текущего элемента
                     if (!theList.isEmpty()) {
                         value = iter1.getCurrent().dData;
                         System.out.println("Returned " + value);
-                    }
-                    else
+                    } else
                         System.out.println("List is empty");
                 }
-                case 'b' -> {
+                case 'b' -> { // Вставка перед текущим элементом
                     System.out.print("Enter value to insert: ");
                     System.out.flush();
                     value = getInt();
                     iter1.insertBefore(value);
                 }
-                case 'a' -> {
+                case 'a' -> { // Вставка после текущего элемента
                     System.out.print("Enter value to insert: ");
                     System.out.flush();
                     value = getInt();
                     iter1.insertAfter(value);
                 }
-                case 'd' -> {
+                case 'd' -> { // Удаление текущего элемента
                     if (!theList.isEmpty()) {
                         value = iter1.deleteCurrent();
                         System.out.println("Deleted " + value);
                     } else {
-                        System.out.println("Invalid entry");
+                        System.out.println("Can’t delete");
                     }
                 }
                 default -> System.out.println("Invalid entry");
@@ -143,12 +142,10 @@ class ListIterator {
     }
 
     static char getChar() throws IOException {
-        String s = getString();
-        return s.charAt(0);
+        return getString().charAt(0);
     }
 
     static int getInt() throws IOException {
-        String s = getString();
-        return Integer.parseInt(s);
+        return Integer.parseInt(getString());
     }
 }
