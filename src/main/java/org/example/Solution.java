2221c100e0dfc61b40e6b3c4a12609e9504359f4
Zhakhongir Esonov <zhakhongi.esonov@x5.ru>
2024-12-15 17:09:56 +0300

    Глава 6

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -8,6 +8,14 @@ public class Solution {
         return n + test(n - 1);
     }
 
+    public boolean isLetterOrDigit(char c) {
+        return (((c >= '0') && (c <= '9')) || (c >= 'a') && (c <= 'z') || (c >= 'A') && (c <= 'Z'));
+    }
+
+    public static char toLowerCase(char c) {
+        return (c >= 'A' && c <= 'Z') ? (char) ('a' + (c - 'A')) : c;
+    }
+
     public static void main(String[] args) {
         System.out.println(test(7));
     }
---
+++ src/main/java/org/example/chapter_6/merge_sort/MergeSort.java
@@ -0,0 +1,38 @@
+package org.example.chapter_6.merge_sort;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arrayA = {23, 47, 81, 95};
+        int[] arrayB = {7, 14, 39, 55, 62, 74};
+        int[] arrayC = new int[arrayA.length + arrayB.length];
+        merge(arrayA, 4, arrayB, 6, arrayC);
+        display(arrayC);
+    }
+
+    // Слияние массивов A и B в массив C
+    public static void merge(int[] arrayA, int sizeA, int[] arrayB, int sizeB, int[] arrayC) {
+        int aDex = 0, bDex = 0, cDex = 0;
+        while (aDex < sizeA && bDex < sizeB) { // Ни один из массивов не пуст
+            if (arrayA[aDex] < arrayB[bDex]) {
+                arrayC[cDex++] = arrayA[aDex++];
+            } else {
+                arrayC[cDex++] = arrayB[bDex++];
+            }
+        }
+        while (aDex < sizeA) { // Массив arrayB пуст,
+            arrayC[cDex++] = arrayA[aDex++]; // в arrayA остались элементы
+        }
+        while (bDex < sizeB) { // Массив arrayA пуст,
+            arrayC[cDex++] = arrayB[bDex++];  // в arrayB остались элементы
+        }
+    }
+
+    // Вывод содержимого массива
+    public static void display(int[] theArray) {
+        for (int j : theArray) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
