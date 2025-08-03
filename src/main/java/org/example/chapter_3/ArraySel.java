856c8dc10c455b6089845c6909926258e9a7abe8
Zhakhongir Esonov <zhakhongi.esonov@x5.ru>
2025-08-03 11:19:44 +0300

    Простая сортировка EZ TECHNOLOGY

--- src/main/java/org/example/chapter_3/ArraySel.java
+++ src/main/java/org/example/chapter_3/ArraySel.java
@@ -26,10 +26,11 @@ public class ArraySel {
         int out, in, min;
         for (out = 0; out < nElems - 1; out++) { // Внешний цикл
             min = out; // Минимум
-            for (in = out + 1; in < nElems; in++) // Внутренний цикл
+            for (in = out + 1; in < nElems; in++) { // Внутренний цикл
                 if (a[in] < a[min]) // Если значение min больше,
                     min = in; // значит, найден новый минимум
-            swap(out, min); // swap them
+            }
+            swap(out, min); // Поменять их местами
         }
     }
 
@@ -39,22 +40,4 @@ public class ArraySel {
         a[two] = temp;
     }
 
-    public static void main(String[] args) {
-        int maxSize = 100; // Размер массива
-        ArraySel arr; // Ссылка на массив
-        arr = new ArraySel(maxSize); // Создание массива
-        arr.insert(77); // Вставка 10 элементов
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(00);
-        arr.insert(66);
-        arr.insert(33);
-        arr.display(); // Вывод элементов
-        arr.selectionSort(); // Сортировка методом выбора
-        arr.display(); // Повторный вывод
-    }
 }
